88aaaf5e7b88f088c299268b3744d9cd7ef2396c
danilo cucharro <danicucharro@outlook.com>
2021-06-24 17:15:40 -0300

    adicionado: página do admin da sala, e o código do arquivo Room.tsx foi refatorado (parte dele foi para o hook useRoom.ts)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from "./pages/NewRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext'
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 
 
@@ -19,6 +20,8 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/rooms/new" component={NewRoom}/>
           <Route path="/rooms/:id" component={Room}/>
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}/>
         </Switch>
       </AuthContextProvider>
           
